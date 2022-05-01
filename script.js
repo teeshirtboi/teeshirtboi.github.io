bb0f9d69001414f161c53eca2d402c9e000521e1
AppleSoda <82600897+teeshirtboi@users.noreply.github.com>
2022-05-01 16:48:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
+    // owl carousel script!
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
